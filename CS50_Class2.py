95240c24510fa5d0388cd2c56559f371be2dc703
ShadowGeek <nshadowens@gmail.com>
2026-05-28 14:36:56 -0400

    Create CS50_Class2.py
    Classwork for week 1 class 2

---
+++ CS50_Class2.py
@@ -0,0 +1,16 @@
+speak = input("What do you want to say? ")
+
+#Make string uppercase
+print(speak.upper())
+
+#Make string lowercase
+print(speak.lower())
+
+#Like Trim command in other languages.
+#Remove leading and trailing spaces.
+print(speak.strip())
+
+#Replace character
+print(speak.replace(" ","_"))
+
+
